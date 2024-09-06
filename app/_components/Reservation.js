42881cc3b2f237e064b48e964c1bc89abb56c6d4
michael-rayner92 <39574445+michael-rayner92@users.noreply.github.com>
2024-09-06 11:57:55 +0200

    End Section 3 - Authentication

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,6 +1,8 @@
+import { auth } from '@/app//_lib/auth';
 import DateSelector from '@/app/_components/DateSelector';
 import ReservationForm from '@/app/_components/ReservationForm';
 import { getBookedDatesByCabinId, getSettings } from '@/app/_lib/data-service';
+import LoginMessage from '@/app/_components/LoginMessage';
 
 export default async function Reservation({ cabin }) {
 	const [settings, bookedDates] = await Promise.all([
@@ -8,10 +10,12 @@ export default async function Reservation({ cabin }) {
 		getBookedDatesByCabinId(cabin.id)
 	]);
 
+	const session = await auth();
+
 	return (
 		<div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
 			<DateSelector bookedDates={bookedDates} cabin={cabin} settings={settings} />
-			<ReservationForm cabin={cabin} />
+			{session?.user ? <ReservationForm cabin={cabin} user={session.user} /> : <LoginMessage />}
 		</div>
 	);
 }
--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,12 @@
+import { auth } from '@/app/_lib/auth';
+
 export const metadata = {
 	title: 'Guest Area'
 };
 
-export default function Page() {
-	return <h2 className="font-semibold text-2xl text-accent-400 mb-7">Welcome, Michael</h2>;
+export default async function Page() {
+	const session = await auth();
+	const firstName = session.user.name.split(' ').at(0);
+
+	return <h2 className="font-semibold text-2xl text-accent-400 mb-7">Welcome, {firstName}</h2>;
 }
